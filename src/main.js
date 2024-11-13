df4116d2d2885e538ac7df9f627134d0a80d2e79
radriann21 <adrianrangelargu@gmail.com>
2024-11-13 17:09:56 -0400

    addded modal and form for tasks

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
+const addTaskButton = document.getElementById('add-task')
+const listTasks = document.getElementById('list-task')
+const formModal = document.getElementById('modal')
+const closeModalButton = document.getElementById('close-modal')
+
+// open/close modal
+addTaskButton.addEventListener('click', () => {
+  formModal.classList.remove('hidden')
+})
+
+closeModalButton.addEventListener('click', () => {
+  formModal.classList.add('hidden')
+})
+
 
